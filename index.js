c91595929647de4a50e55bd3dffb9ca25117c72d
KYGAS <45013496+KYGAS@users.noreply.github.com>
2020-03-05 00:00:37 +0100

    Added auto update

--- index.js
+++ index.js
@@ -6,41 +6,39 @@ module.exports = function ValkFastRB(mod) {
 	let command = mod.command;
 	let gameId = 0n,
 		model = 0,
-		job = 0,
-		enabled = false,
-		delay = 700;
+		job = 0;
 	
 	comand.add('valkrb', (arg,delay) =>{
-		mod.saveSettings();
 		if(arg == undefined){
-			enabled = !enabled;
+			mod.settings.enabled = !mod.settings.enabled;
 			command.message("Quick Runeburst module is now : " + (enabled?"Enabled":"Disabled"));
 		}else if(arg == 'on'){
-			enabled = true;
+			mod.settings.enabled = true;
 			command.message("Quick Runeburst module is now : " + (enabled?"Enabled":"Disabled"));
 		}else if(arg == 'off'){
-			enabled = false;
+			mod.settings.enabled = false;
 			command.message("Quick Runeburst module is now : " + (enabled?"Enabled":"Disabled"));
 		}else if(arg == 'delay'){
 			if(isNaN){
 				command.message("Delay has to be a number.");
 				return;
 			}
-			delay = Number(delay);
+			mod.settings.delay = Number(delay);
 		}else{
 			command.message("Unknown command. Available commands are : on|off|delay {Value}");
 		}
+		mod.saveSettings();
 	});
 	
 	mod.hook('S_LOGIN', 'raw', { order : Infinity }, event => {
 	    gameId = mod.game.me.gameId;
 	    model = mod.game.me.templateId;
 	    job = (model - 10101) % 100;
-	    enabled = [JOB_VALK].includes(job);
+	    mod.settings.enabled = [JOB_VALK].includes(job);
 	});
 
 	mod.hook('S_ACTION_STAGE', 9, {order: -Infinity, filter: {fake: null}}, event => {
-		if(!enabled) return;
+		if(!mod.settings.enabled) return;
 		if(event.gameId != gameId) return;
 		
 	    if(skillIDs.includes(event.skill.id)) event.skill.id -= 30;
@@ -55,12 +53,12 @@ module.exports = function ValkFastRB(mod) {
 					type: 999999,
 					id: event.id,
 				});
-	    	}, delay);
+	    	}, mod.settings.delay);
 	    }
 	});
 
 	mod.hook('S_ACTION_END', 5, {order: -Infinity, filter: {fake: null}}, event => {
-		if(!enabled) return
+		if(!mod.settings.enabled) return
 			if(!event.gameId == gameId) return;
 				if(([].concat(skillIDs,cancelIDs)).includes(event.skill.id)) return ((event.type==999999)?event.type=4:return false);
 	});
